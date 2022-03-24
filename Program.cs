7bfcea8b202bdf14d4e901ec21078c8bcb7de54e
Jill Oakes <jericho_rules@hotmail.com>
2022-03-24 11:58:40 -0400

    Test 2

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace MethodsWithLoopsAndConditionals
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello solar system");
+            Console.WriteLine("Hello galaxy");
 
 
         }
